cf10087143b5df638790a4cbc12b073f1f861ff1
Lupus590 <lupussolitarius590@gmail.com>
2019-04-11 23:30:09 +0100

    stuff

--- treeFarm/server.lua
+++ treeFarm/server.lua
@@ -2,7 +2,7 @@ local rednetUtils = require("utils.rednetUtils")
 
 rednet.host(rednetUtils.protocolName, rednetUtils.serverName)
 
-
+-- no server, all the turtles track the build and broadcast the info
 
 
 -- track build stuff
