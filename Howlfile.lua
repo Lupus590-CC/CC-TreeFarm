67f9b1e18fe174e9132a64cc588a390cdc867805
Lupus590 <lupussolitarius590@gmail.com>
2019-04-17 20:52:35 +0100

    stuff

--- Howlfile.lua
+++ Howlfile.lua
@@ -3,10 +3,26 @@ Tasks:Default "build"
 
 Tasks:clean()
 
-Tasks:minify "minify" {
-	input = "build/treeFarm.lua",
-	output = "build/treeFarm.min.lua",
+Tasks:minify "minify" { -- TODO: can I wildcard this?
+	input = "build/treeFarm.un.lua",
+	output = "build/treeFarm.min.un.lua",
 }
+
+-- TODO: find out how to add license to start of output file (link sent to desktop)
+Tasks:Task "license" (function(_, _, file, dest)
+		local fs = require "howl.platform".fs
+		local contents = table.concat {
+		"--[[\n"]],
+		fs.read(File "License.txt"),
+		"\n]]\n",
+		fs.read(File(file)),
+		}
+		
+		fs.write(File(dest), contents)
+		end)
+		:maps("wild:build/*.un.lua", "wild:build/*.lua")
+		:description "Prepends license"
+
 
 -- TODO: separate into multiple tasks, farm, build, furnace, remote, combine
 -- they may be best to put in separate folders, in which case it may be a good idea to
@@ -15,7 +31,7 @@ Tasks:minify "minify" {
 Tasks:require "main" {
 	include = "treeFarm/*.lua",
 	startup = "treeFarm/main.lua",
-	output = "build/treeFarm.lua",
+	output = "build/treeFarm.un.lua",
 }
 
-Tasks:Task "build" { "clean", "minify" } :Description "Main build task"
+Tasks:Task "build" { "clean", "minify", "license" } :Description "Main build task"
