f12666950d5caeeed58064d1ffd5d4188a87cc85
Lupus590 <lupussolitarius590@gmail.com>
2019-04-18 17:55:15 +0100

    stuff

--- treeFarm/utils/init.lua
+++ treeFarm/utils/init.lua
@@ -2,11 +2,22 @@ local rednetUtils = require("rednetUtils")
 local itemUtils = require("itemUtils")
 local nav = require("nav")
 
+-- tODO: move to nav?
+local function fuelCheck() -- TODO: fuel check
+	-- if fuel is low
+		-- find fuel in inventory
+		-- refuel
+		-- if fuel still low
+			-- get more fuel
+	
+end
+
 
 local utils = {
   rednetUtils = rednetUtils,
   itemUtils = itemUtils,
   nav = nav,
+  fuelCheck = fuelCheck
 }
 
 return utils
